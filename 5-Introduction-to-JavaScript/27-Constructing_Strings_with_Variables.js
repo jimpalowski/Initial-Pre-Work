b4f6ff6d4539671a2823d449eb593986001851d0
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:23:55 -0700

    Adds code for 27th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// We set a variable of myName and a new string of myStr so we can concatenate myName into the sentence.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Jim";
+var myStr = "My name is " + myName + " and I am well!";
