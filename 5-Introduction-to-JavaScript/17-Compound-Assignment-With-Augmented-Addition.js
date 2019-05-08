4a7c712817173f892fb762b0e079450f1691de33
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:31:03 -0700

    Adds code for 17th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// By using the += operator we are able to do less work and get the same results instead of repeating yourself.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
