37501a72f8fa37831c10d19d9a24e914b2893c96
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:59:29 -0700

    Adds code for 34rd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// We use bracket notation again to find the second to last letter in the lastName variable starting at -2.
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
