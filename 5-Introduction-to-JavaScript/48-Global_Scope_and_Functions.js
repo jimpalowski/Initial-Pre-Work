b8d73e07a8a6af008382d821609b8cb50f536a8d
Jim Palowski <palowskijim@gmail.com>
2019-05-08 03:21:21 -0700

    Adds code for 48th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,21 @@
+// Outside of functions we use var to define it globally but inside functions we dont always need to do it.
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
