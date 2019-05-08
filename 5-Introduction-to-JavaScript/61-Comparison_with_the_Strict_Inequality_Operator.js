ccca8803257d7914cf86b3d408342de75bd21b5f
Jim Palowski <palowskijim@gmail.com>
2019-05-08 13:07:25 -0700

    Adds code for 61st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// We use the strictly not equal operator so that we only get the value and nothing else to return.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+  
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
