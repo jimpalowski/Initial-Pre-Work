fe3726bfc69a9603475cc9095f3fea886cef7279
Jim Palowski <palowskijim@gmail.com>
2019-05-08 13:12:03 -0700

    Adds code for 62nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -0,0 +1,16 @@
+// We use the greater than operator to get a value that is greater than 10 or 100 to return a string.
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+  
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(10);
