a8a8bf93d511b75d1f9d05f508cca69ac1549cec
Jim Palowski <palowskijim@gmail.com>
2019-05-08 03:32:05 -0700

    Adds code for 51st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+// Made a function that takes a number and multiplies a given number by five.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(num){
+  return num * 5;
+}
+
+timesFive(10)
+
+console.log(minusSeven(10));
