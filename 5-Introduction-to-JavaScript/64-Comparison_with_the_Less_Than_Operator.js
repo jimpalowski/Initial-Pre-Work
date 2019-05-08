a6d1d3a86ff5811e43d9c37816e6a8a64d15a1ce
Jim Palowski <palowskijim@gmail.com>
2019-05-08 13:49:56 -0700

    Adds code for 64th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -0,0 +1,16 @@
+// We set our if statements to check if our value is less than 25 or 55, then we return a string.
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+  
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
