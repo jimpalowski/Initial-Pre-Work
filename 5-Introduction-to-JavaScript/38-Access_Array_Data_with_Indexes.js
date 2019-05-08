4b2008bee563631474663673f99ae6ccfd170dea
Jim Palowski <palowskijim@gmail.com>
2019-05-08 02:19:59 -0700

    Adds code for 38th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// Made a new variable to retrieve the first value of myArray using bracket notation.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0]
