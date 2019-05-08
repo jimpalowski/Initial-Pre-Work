4699d9ddd88b9d59d075403ef43e2c2f9102ce76
Jim Palowski <palowskijim@gmail.com>
2019-05-08 15:04:35 -0700

    Adds code for 72nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,24 @@
+// In this exercise we made a function that will pass in pars and strokes as numbers and for each score we return a string.
+
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if (strokes == 1){
+    return "Hole-in-one!";
+  } else if (strokes <= par -2){
+    return "Eagle";
+  } else if (strokes == par -1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par +1) {
+    return "Bogey";
+  } else if (strokes == par +2) {
+    return "Double Bogey";
+  } else {
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+// Change these values to test
+golfScore(5, 4);
