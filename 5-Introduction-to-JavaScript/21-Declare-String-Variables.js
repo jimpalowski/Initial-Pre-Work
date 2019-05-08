4fe9fa4b0d7f2d3033388488b6cb0a2bc4e6a05c
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:42:16 -0700

    Adds code for 21st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+// We declare two string variables of my first and last name.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Jim";
+var myLastName = "Palowski";
