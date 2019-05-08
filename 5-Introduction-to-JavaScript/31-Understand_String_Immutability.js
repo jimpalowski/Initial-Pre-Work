66efa31357aea8a7e6f5c8d273dd60bf44c03849
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:40:42 -0700

    Adds code for 31st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// We fixed the string variable and since strings are immutable we changed the string.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
