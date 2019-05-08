8c6bd37e748fcff3b8f43c82c3e3f6246b5b5262
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:22:13 -0700

    Adds code for 82nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// Using the bracket notation is another way for us to pull the values from the object.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
