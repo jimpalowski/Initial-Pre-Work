4f0b5ef29cb663bfacf7c9f99d10d84afa3ef687
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:20:05 -0700

    Adds code for 81st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+// We used the dot notation to specifially pull the value from the object.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
