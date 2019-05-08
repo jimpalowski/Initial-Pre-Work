e04e24201d76b7886cc54b97468f80b8420e9435
Jim Palowski <palowskijim@gmail.com>
2019-05-08 12:57:02 -0700

    Adds code for 60th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// We set an inequality operator so that if our value does not equal 99 then we return.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
