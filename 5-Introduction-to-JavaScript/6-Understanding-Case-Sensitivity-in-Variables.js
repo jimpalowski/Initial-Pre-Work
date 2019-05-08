f6083e52f28d38d52089c982f95667e577c00055
Jim Palowski <palowskijim@gmail.com>
2019-05-07 23:55:33 -0700

    Adds code for 6th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// Here we set the variables to use proper camel case.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
