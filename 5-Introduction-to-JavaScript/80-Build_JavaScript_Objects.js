20c1e0cf6fa94cebfd4ca1e03c825945cb676097
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:17:21 -0700

    Adds code for 80th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+// We made an object with four properties.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Rock",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Ice"]
+  
+  
+  
+};
