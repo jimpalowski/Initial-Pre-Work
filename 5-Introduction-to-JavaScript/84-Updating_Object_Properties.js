c23783241143dd56501a3808b9b4176239b9bfb9
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:30:58 -0700

    Adds code for 84th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -0,0 +1,22 @@
+// We update myDog name by using the name object property and setting a new name with a string
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder";
