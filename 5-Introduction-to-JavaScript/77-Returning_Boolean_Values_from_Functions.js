8aa50623926ab06dd1853c014ce95de6fa928b5b
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:03:48 -0700

    Adds code for 77th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// We return a boolean by return a is less than or equal to b thus returning true.
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b;
+}
+
+// Change these values to test
+isLess(10, 15);
