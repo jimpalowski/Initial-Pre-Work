12b2fc5010d16e1f4d89b16873db1ca8f17e6a3f
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:19:31 -0700

    Adds code for 26th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+// We set a string variable and used the plus equals operator so we can concatenate a sentence together.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
