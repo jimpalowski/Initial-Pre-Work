b5e6e4bdee64b3b4e28cd5ac3126a8189a0a6b2c
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:10:42 -0700

    Adds code for 25th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,9 @@
+// By concatening strings in our variable we are able to add two strings together.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
+
