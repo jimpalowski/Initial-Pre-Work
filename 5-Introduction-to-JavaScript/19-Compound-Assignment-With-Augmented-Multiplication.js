8bed67ad41afb252691c47f4bf0a6bb58775928f
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:36:46 -0700

    Adds code for 19th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/19-Compound-Assignment-With-Augmented-Multiplication.js
@@ -0,0 +1,11 @@
+// With the *= operator we are able to multiply variables with a given number to multiply.
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only modify code below this line
+
+a *= 5;
+b *= 3;
+c *= 10;
