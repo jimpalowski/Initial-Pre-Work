c7792fc969dd61a46f6b647c0fa3772582f79493
Jim Palowski <palowskijim@gmail.com>
2019-05-08 03:12:10 -0700

    Adds code for 47th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,14 @@
+// Made a function that takes two arguments and adding them together to return the sum.
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(sum1, sum2){
+  console.log(sum1 + sum2);
+}
+functionWithArgs(10, 13);
