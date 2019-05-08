8eed01d889e3c7f7b0713ea6be7217a7964d68ef
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:33:33 -0700

    Adds code for 85th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// We used the dot notation to add a new property to our object.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "woof";
