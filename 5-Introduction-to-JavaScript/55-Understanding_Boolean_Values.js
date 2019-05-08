164b70a29194ca42e5d7de59a39772cb3b14b3e6
Jim Palowski <palowskijim@gmail.com>
2019-05-08 12:28:41 -0700

    Adds code for 55th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// We changed to boolean to be set to true.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
