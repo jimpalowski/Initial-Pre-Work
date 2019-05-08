b54c5b2d5f827426f3b853de5d3d0f25676e7fe9
Jim Palowski <palowskijim@gmail.com>
2019-05-08 13:58:35 -0700

    Adds code for 65th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/65-Comparison_with_the_Less_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+// We are using the less than or equal to operator so we can get the exact value to match 12 or 24 to return a string.
+
+function testLessOrEqual(val) {
+  if (val <= 12) {  // Change this line
+    return "Smaller Than or Equal to 12";
+  }
+  
+  if (val <= 24) {  // Change this line
+    return "Smaller Than or Equal to 24";
+  }
+
+  return "More Than 24";
+}
+
+// Change this value to test
+testLessOrEqual(10);
