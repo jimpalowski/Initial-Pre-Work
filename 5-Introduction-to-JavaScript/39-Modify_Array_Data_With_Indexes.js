18aab004e52c70caf78aba295143deec3095876d
Jim Palowski <palowskijim@gmail.com>
2019-05-08 02:23:53 -0700

    Adds code for 39th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/39-Modify_Array_Data_With_Indexes.js
@@ -0,0 +1,11 @@
+// Arrays are mutable so we are able to change the first value of myArray with bracket notation.
+
+// Example
+var ourArray = [18,64,99];
+ourArray[1] = 45; // ourArray now equals [18,45,99].
+
+// Setup
+var myArray = [18,64,99];
+
+// Only change code below this line.
+myArray[0] = 45;
