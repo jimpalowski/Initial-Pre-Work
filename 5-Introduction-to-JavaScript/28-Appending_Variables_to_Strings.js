2de62258366cb303487d5c0321eb737556378857
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:29:00 -0700

    Adds code for 28th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,12 @@
+// We defined two string variables and used the += operator to make a complete sentence.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "Fun";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
