93cbd44345923e7f115c41ad1ac271b699ce203e
Jim Palowski <palowskijim@gmail.com>
2019-05-08 02:43:38 -0700

    Adds code for 41st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -0,0 +1,13 @@
+// With the .push function we are able to add an additional array to an already existing array.
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3])
+;
