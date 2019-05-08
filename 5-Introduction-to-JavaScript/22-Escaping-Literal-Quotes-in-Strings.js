e08c605d4b1d0c685b0a6f000d595a09fbec23d0
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:47:20 -0700

    Adds code for 22nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+// In this variable of myStr we set a string to have literal quotes within the sentence.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
