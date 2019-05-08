a4c5d62b72bad527a4f46062d6ab442a4599c6b1
Jim Palowski <palowskijim@gmail.com>
2019-05-08 02:09:22 -0700

    Adds code for 37th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+// We made a variable with an array nested with two other arrays making it a Multi-dimensional Array.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Hello", 3], ["World", 4]];
