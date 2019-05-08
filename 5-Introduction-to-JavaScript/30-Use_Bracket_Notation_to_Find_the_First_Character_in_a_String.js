1029d84fb6b905805344ed35eb54e181888aab95
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:34:49 -0700

    Adds code for 30th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,14 @@
+// We use bracket notation to find the the first word in the lastName variable which is "L".
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
