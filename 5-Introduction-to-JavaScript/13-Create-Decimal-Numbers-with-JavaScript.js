98ef3e8644e7ba45ab7d8e0c719415ea9dc37e59
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:20:03 -0700

    Adds code for 13th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+// In variables we are also able to give it the value of decimals also known as float point numbers.
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
