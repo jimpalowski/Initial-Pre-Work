5919cdf6cf5537e91f99c1573908029097892ae9
Jim Palowski <palowskijim@gmail.com>
2019-05-08 00:33:45 -0700

    Adds code for 18th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,12 @@
+// We are using the -= operator to subract from the variable same as adding from last excercise.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
+
