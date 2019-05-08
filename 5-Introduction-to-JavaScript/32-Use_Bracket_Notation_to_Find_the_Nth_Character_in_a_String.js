e168e2300efeff5a2adcb1e31dc4f2185c6a88f1
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:46:11 -0700

    Adds code for 32nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// We used bracket notation to retrieve the third letter of lastName which is "V".
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
