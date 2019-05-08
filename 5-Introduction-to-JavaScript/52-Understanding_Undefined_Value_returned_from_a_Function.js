8bdb18bcdb3adf4110e01c973f3a7d18e5f9ae5f
Jim Palowski <palowskijim@gmail.com>
2019-05-08 03:37:50 -0700

    Adds code for 52nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+// Made a function to add the global variable to add five to its count.
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive(){
+  sum += 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
