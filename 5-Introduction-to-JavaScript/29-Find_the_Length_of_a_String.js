cd3a8662d2437524b2c7a322980a79401aaa0e88
Jim Palowski <palowskijim@gmail.com>
2019-05-08 01:32:37 -0700

    Adds code for 29th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+// The .length property gives us the length of the exact string property.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
