d110f2fab9310f9dfb7a582866e6c04617cc01e1
Jim Palowski <palowskijim@gmail.com>
2019-05-08 16:25:47 -0700

    Adds code for 83rd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,14 @@
+// We assigned a variable to value in the object and that way we are able to use the bracket
+// notation on our variable.
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
