6adb98336ef30036ab4ed01e01af10344a2b098a
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:00:38 -0700

    Adds code for 94th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+// We start the iteration at 1 and loop through until i is greater than or equal to 5 adding to the new array.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+for (var i = 1; i <= 5; i++){
+  myArray.push(i);
+}
+// Only change code below this line.
+
