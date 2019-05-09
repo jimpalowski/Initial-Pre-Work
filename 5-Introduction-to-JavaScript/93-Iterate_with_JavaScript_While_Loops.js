3e13c8ff6dcf4d34bc74d62633e57f2699c95a42
Jim Palowski <palowskijim@gmail.com>
2019-05-08 17:54:50 -0700

    Adds code for 93rd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,11 @@
+// We made a while loop to add the numbers 0 through 4 to the array.
+
+// Setup
+var myArray = [];
+var i = 0
+while(i < 5){
+  myArray.push(i);
+  i++
+};
+// Only change code below this line.
+
