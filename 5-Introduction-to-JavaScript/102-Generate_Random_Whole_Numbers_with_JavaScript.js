2496aa13198299e12d102f88d9493749dc1282c0
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:37:49 -0700

    Adds code for 102nd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+// Using Math.floor and .random we are able to generate a random number 0-9.
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
