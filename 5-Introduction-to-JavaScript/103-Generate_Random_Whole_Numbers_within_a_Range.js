95ade59f0fe9d7b5369ed53d218abe58b32fabb1
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:42:16 -0700

    Adds code for 103rd part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/103-Generate_Random_Whole_Numbers_within_a_Range.js
@@ -0,0 +1,20 @@
+// We were able to generate a random set of numbers from a set minimum and max.
+
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
