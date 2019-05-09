fc3804952a17382816e218d72f8ead62cda733f5
Jim Palowski <palowskijim@gmail.com>
2019-05-08 17:08:51 -0700

    Adds code for 89th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+We added a new album in our myMusic object.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Baby Bash",
+    "title": "Cyclone",
+    "release_year": 2013,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
