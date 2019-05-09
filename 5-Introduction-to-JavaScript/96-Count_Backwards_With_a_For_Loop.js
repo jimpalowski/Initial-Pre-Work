0a259bd97db042d5562afeea535e8b70fa205447
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:14:54 -0700

    Adds code for 96th part of JS module and summary.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+// We looped through the numbers 1-10 backwords and only added the odd numbers to our array.
+
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+  for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+// Only change code below this line.
+
 
