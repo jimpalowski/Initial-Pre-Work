8541ed37088df96a1ca22a1ee5c8ce5ccfa2ef00
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:33:39 -0700

    Adds code for 101st part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// We made a function to generate a random number.
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
