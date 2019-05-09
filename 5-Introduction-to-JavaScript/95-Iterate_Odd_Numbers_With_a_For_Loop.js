199ed8b332805b4b90b5f7c0d544ea614e4c0ce6
Jim Palowski <palowskijim@gmail.com>
2019-05-08 18:05:00 -0700

    Adds code for 95th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+// We made a for loop to add odd numbers from 1-10 and push the numbers to an array.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for ( var i = 1; i < 10; i+=2){
+  myArray.push(i);
+}
