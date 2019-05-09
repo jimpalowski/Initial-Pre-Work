0383962fd50d2261c6ef63186e13fe3be22d62d5
Jim Palowski <palowskijim@gmail.com>
2019-05-08 19:13:49 -0700

    Adds code for 106th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// We use this function to check if both parameters are equal to each other with if and else conditionals.
+
+function checkEqual(a, b) {
+  return a === b ? true: false;
+}
+
+checkEqual(1, 2);
