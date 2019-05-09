4b4e5dd1447270bce3159cf55d7ff7907230b29e
Jim Palowski <palowskijim@gmail.com>
2019-05-08 17:18:15 -0700

    Adds code for 90th part of JS module and summary.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// With the bracket notation we were able to navigate through the car to get to the glove box.
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside['glove box'] ; // Change this line
