7c0a826adca0d75b4ab6e82fad01825074df4954
Jim Palowski <palowskijim@gmail.com>
2019-05-09 23:47:33 -0700

    Adds code for 2nd part of ES6 module and summary.

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+// In this function we are testing the scope of i because defining it with let makes the scope limited.
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
