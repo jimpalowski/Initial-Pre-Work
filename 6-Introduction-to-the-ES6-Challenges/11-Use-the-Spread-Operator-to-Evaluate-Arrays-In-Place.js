86a0badcc2c502dd7923051c6c99cc5487613738
Jim Palowski <palowskijim@gmail.com>
2019-05-10 01:10:12 -0700

    Adds code for 11th part of ES6 module and summary.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+// We used the spread operator to copy all the contents of arr1 into arr2.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
