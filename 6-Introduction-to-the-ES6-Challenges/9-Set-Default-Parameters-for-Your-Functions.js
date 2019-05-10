cae72893bf07d7ef0ddded8c01030a2115393dc0
Jim Palowski <palowskijim@gmail.com>
2019-05-10 00:58:12 -0700

    Adds code for 9th part of ES6 module.

---
+++ 6-Introduction-to-the-ES6-Challenges/9-Set-Default-Parameters-for-Your-Functions.js
@@ -0,0 +1,10 @@
+// In this function we set a value of 1 in the parameter is when it returns it already has added one time.
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value = 1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
