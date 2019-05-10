afc4f9d798f8849a0ed86c85a112b910040bcc3d
Jim Palowski <palowskijim@gmail.com>
2019-05-10 02:27:45 -0700

    Adds code for 22nd part of ES6 module and summary.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// We import the capitalizeString from the string_functions library so we can use it in our project.
+
+"use strict";
+import { capitalizeString } from "string_functions";
+capitalizeString("hello!");
